8b2016ce93622c84c5ab77559b46608c14c3f7bd
taha127 <tahadehghani127@gmail.com>
2025-04-14 14:22:56 +0330

    update urls.py: change account path to accounts and include allauth URLs

--- config/urls.py
+++ config/urls.py
@@ -20,5 +20,5 @@ from django.urls import path ,include
 urlpatterns = [
         path('admin/', admin.site.urls),
         path('',include('home.urls')),
-        path('account/', include('django.contrib.auth.urls')),
+        path('accounts/', include('allauth.urls')),
 ]

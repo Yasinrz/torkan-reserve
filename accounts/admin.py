b627465f10d86ad8e40bd2a73b84a7f7313d1724
taha127 <tahadehghani127@gmail.com>
2025-04-20 12:47:09 +0330

    add admin.py: implement custom UserAdmin with phone number and custom forms

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.translation import gettext_lazy as _
+from .models import User
+from .forms import CustomUserCreationForm, CustomUserChangeForm
 
-# Register your models here.
+
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    fieldsets = (
+        (None, {'fields': ('phone_number', 'password')}),
+        (_('Personal info'), {'fields': ('name',)}),
+        (_('Permissions'), {'fields': ('is_staff', 'is_superuser',)}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('phone_number', 'name', 'password1', 'password2', 'is_superuser', 'is_staff',),
+        }),
+    )
+    list_display = ('phone_number', 'name', 'is_active', 'date_joined',)
+    search_fields = ('phone_number', 'name')
+    ordering = ('date_joined',)

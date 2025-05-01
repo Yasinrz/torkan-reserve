a21a228a360246c21d8bc426b6a6719957c41b25
taha127 <tahadehghani127@gmail.com>
2025-05-01 09:14:13 +0330

    add forms.py: enhance RequestReservationForm to support Jalali date input

--- home/forms.py
+++ home/forms.py
@@ -1,10 +1,31 @@
+
 from django import forms
 from .models import RequestReservation
+from jalali_date.fields import JalaliDateField
+from jalali_date.widgets import AdminJalaliDateWidget
+from django_jalali.admin.widgets import AdminjDateWidget
+from django_jalali.forms import jDateField
+import jdatetime
+
 
 class RequestReservationForm(forms.ModelForm):
+    # suggested_reservation_date = jDateField(widget=AdminjDateWidget)
     class Meta:
         model = RequestReservation
         fields = ('suggested_reservation_date',)
-        widgets = {
-            'suggested_reservation_date': forms.DateInput(attrs={'class': 'form-control', 'type': 'date'}),
-        }
+
+    def __init__(self, *args, **kwargs):
+        super(RequestReservationForm, self).__init__(*args, **kwargs)
+
+        self.fields['suggested_reservation_date'] = JalaliDateField(
+            label='Jalali Date',
+            widget=AdminJalaliDateWidget
+        )
+        self.fields['suggested_reservation_date'].widget.attrs.update({'class': 'data-jdp-only-date'})
+
+    def clean_suggested_reservation_date(self):
+        data = self.cleaned_data['suggested_reservation_date']
+        if isinstance(data, jdatetime.date):
+            # Convert Jalali date to Gregorian date
+            return data.togregorian()
+        return data

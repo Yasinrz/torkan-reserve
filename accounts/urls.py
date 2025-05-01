e70262e6266522c0248d22ccb2426584592674f9
taha127 <tahadehghani127@gmail.com>
2025-05-01 09:46:26 +0330

    update urls.py: remove unused reservation API path and clean up urlpatterns

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,6 +5,5 @@ urlpatterns = [
     path('login/', views.phone_number_view, name='login'),
     path('verify/', views.verify, name='code_view'),
     path('welcome/', views.welcome, name='welcome'),
-    path('calendar/', views.calendar, name='calendar'),
 
 ]
--- home/urls.py
+++ home/urls.py
@@ -2,9 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    #path('', views.home, name='home'),
     path('', views.home, name='home'),
-    path('api/reservations/' ,views.reservation_api, name='reservation_api' ),
-
-
+    path('calendar/', views.calendar, name='calendar'),
 ]

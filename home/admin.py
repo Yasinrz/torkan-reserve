589477077f94c455067245557062ac8a98f62f9a
taha127 <tahadehghani127@gmail.com>
2025-05-08 09:55:36 +0330

    change widgets for suggested_reservation_time and label
    fix display calendar in admin panel

--- home/admin.py
+++ home/admin.py
@@ -1,25 +1,17 @@
 from django.contrib import admin
-from django.db import models
-from django_jalali.admin.widgets import AdminjDateWidget
-from .models import Time, Operation, OperationSetting, RequestReservation
-from django_jalali.admin.filters import JDateFieldListFilter
 from jalali_date.admin import ModelAdminJalaliMixin
 from jalali_date import date2jalali
+from .models import Time, Operation, OperationSetting, RequestReservation
 from .utils import send_reservation_sms
-from .forms import RequestReservationForm
 
 
 @admin.register(Time)
-class TimeAdmin(admin.ModelAdmin):
-    list_display = ('request_reservation', 'shamsi_date', 'start_session', 'volume', 'unit')
+class TimeAdmin(ModelAdminJalaliMixin, admin.ModelAdmin):
+    list_display = ('request_reservation', 'fix_reserved_date', 'start_session', 'volume', 'unit')
     search_fields = ('request_reservation__user__name', 'request_reservation__user__phone_number')
-    list_filter = (('shamsi_date', JDateFieldListFilter),)
-    ordering = ('shamsi_date',)
-    readonly_fields = ('date_time_reserved',)
+    ordering = ('-fix_reserved_date',)
+    readonly_fields = ('date_time_created',)
     autocomplete_fields = ('request_reservation',)
-    formfield_overrides = {
-        models.DateField: {'widget': AdminjDateWidget},
-    }
 
     def save_model(self, request, obj, form, change):
         super().save_model(request, obj, form, change)
@@ -27,9 +19,12 @@ class TimeAdmin(admin.ModelAdmin):
         # get data to send SMS
         phone = obj.request_reservation.user.phone_number
         name = obj.request_reservation.user.name
-        date = obj.shamsi_date.strftime('%Y/%m/%d')
-
+        date = obj.fix_reserved_date.strftime('%Y/%m/%d')
         send_reservation_sms(phone, name, date)
+
+    @admin.display(description='fixed_reserved_date')
+    def get_jalali_date(self, obj):
+        return date2jalali(obj.date).strftime('%Y/%m/%d')
 
 
 @admin.register(Operation)

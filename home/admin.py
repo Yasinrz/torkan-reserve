ff5d3bd9fb3b2a9735df15198f8c95bd4db4144a
taha127 <tahadehghani127@gmail.com>
2025-06-02 10:39:21 +0330

    fix: update TimeAdmin and RequestReservationAdmin for improved date display and translation

--- home/admin.py
+++ home/admin.py
@@ -9,7 +9,7 @@ from django.utils.translation import gettext_lazy as _
 
 @admin.register(Time)
 class TimeAdmin(ModelAdminJalaliMixin, admin.ModelAdmin):
-    list_display = ('request_reservation', 'format_date', 'start_session', 'volume', 'unit')
+    list_display = ('trans_request_reservation_date', 'format_date', 'start_session', 'volume', 'unit')
     search_fields = ('request_reservation__user__name', 'request_reservation__user__phone_number')
     ordering = ('-fix_reserved_date',)
     autocomplete_fields = ('request_reservation',)
@@ -23,14 +23,15 @@ class TimeAdmin(ModelAdminJalaliMixin, admin.ModelAdmin):
         miladi_date = form.cleaned_data['fix_reserved_date']
         shamsi_date = jdatetime.date.fromgregorian(date=miladi_date)
         date = shamsi_date.strftime('%Y/%m/%d')
-
-
         send_reservation_sms(phone, name, date)
 
 
     @admin.display(description=_('Reservation date'))
     def format_date(self, obj):
         return date2jalali(obj.fix_reserved_date).strftime('%Y/%m/%d')
+    @admin.display(description=_('Request reservation date'))
+    def trans_request_reservation_date(self, obj):
+        return obj.request_reservation.user
 
 
 @admin.register(Operation)
@@ -47,10 +48,13 @@ class OperationSettingAdmin(admin.ModelAdmin):
 
 @admin.register(RequestReservation)
 class RequestReservationAdmin(ModelAdminJalaliMixin, admin.ModelAdmin):
-    list_display = ['user', 'datetime_created', 'suggested_reservation_date', 'status', ]
+    list_display = ['user', 'datetime_created_jalali', 'suggested_jalali_date', 'status', ]
     search_fields = ['user__name', 'user__phone_number']
     ordering = ['-datetime_created', ]
 
-    @admin.display(description='suggested_reservation_date')
-    def get_jalali_date(self, obj):
-        return date2jalali(obj.date).strftime('%Y/%m/%d')
+    @admin.display(description=_('suggested_reservation_date'))
+    def suggested_jalali_date(self, obj):
+        return date2jalali(obj.suggested_reservation_date).strftime('%Y/%m/%d')
+    @admin.display(description=_('datetime_created'))
+    def datetime_created_jalali(self, obj):
+        return date2jalali(obj.datetime_created.date()).strftime('%Y/%m/%d')

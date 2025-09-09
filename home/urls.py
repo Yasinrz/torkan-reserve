ed85a7dfb697b4b3cc5f4bc3827073abb0c79333
taha127 <tahadehghani127@gmail.com>
2025-09-10 02:55:55 +0330

    Changing url for contact us to render create ticket form

--- home/urls.py
+++ home/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from . import views
+from accounts.views import custom_create_ticket
 
 urlpatterns = [
     path('', views.home, name='home'),
     path('calendar/', views.calendar, name='calendar'),
     path('dashbord/', views.dashbord, name='dashbord'),
-    path('contact_us/',views.contact_us, name='contact_us'),
+    path('contact_us/', lambda request: custom_create_ticket(request, "home/contact_us.html"), name='contact_us'),
 ]

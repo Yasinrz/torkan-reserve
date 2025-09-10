091b2a74fccdbe8226232f4f97de34229610bec1
taha127 <tahadehghani127@gmail.com>
2025-09-11 02:08:11 +0330

    Use session for customer create ticket view and login required alert status. Improve clean code

--- accounts/views.py
+++ accounts/views.py
@@ -1,11 +1,10 @@
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import get_user_model
 from django.contrib.auth import authenticate, login
-from django.shortcuts import render, redirect ,get_object_or_404
-from django.urls import reverse
+from django.shortcuts import render, redirect, get_object_or_404
 from django.http import JsonResponse
 from django.core.paginator import Paginator
-from home.models import Time ,RequestReservation
+from home.models import Time, RequestReservation
 from .forms import *
 from .models import *
 from random import randint
@@ -15,9 +14,6 @@ from django.views.decorators.cache import never_cache
 from config.settings import DEBUG
 from django.contrib.admin.views.decorators import staff_member_required
 from django.contrib import messages
-
-
-
 
 
 def phone_number_view(request):
@@ -31,6 +27,7 @@ def phone_number_view(request):
             request.session['phone_number'] = phone_number
             request.session['verification_code'] = token
             request.session['name'] = name
+            # Just for don`t use SMS in development mode
             if not DEBUG:
                 send_code(phone_number, token)
             print(f" $--------------------$ {token} $--------------------$ ")
@@ -53,17 +50,19 @@ def verify(request):
 
             if entered_code == stored_code:
                 # پیدا کردن یا ساختن کاربر
-                user, created = User.objects.get_or_create(phone_number=phone_number)
-                
+                user, created = User.objects.get_or_create(
+                    phone_number=phone_number)
+
                 if created:
                     user.name = name
                     user.save()
 
                 # احراز هویت و لاگین کردن کاربر
-                user = authenticate(request, phone_number=phone_number, verification_code=entered_code)
+                user = authenticate(
+                    request, phone_number=phone_number, verification_code=entered_code)
                 if user is not None:
                     login(request, user)
-                    
+
                     # پاک کردن داده‌های سشن
                     request.session.pop('verification_code', None)
                     request.session.pop('phone_number', None)
@@ -73,7 +72,7 @@ def verify(request):
                     if user.is_staff:
                         return redirect('employee_panel')
                     else:
-                        return redirect('calendar')
+                        return redirect('custom_panel')
                 else:
                     form.add_error('verification_code', 'خطا در احراز هویت')
             else:
@@ -84,27 +83,26 @@ def verify(request):
     return render(request, 'registration/verify.html', {'form': form})
 
 
-
-
-
-
 def welcome(request):
     return render(request, 'registration/welcome.html')
 
 
 def custom_permission_denied_view(request, exception):
-    return render(request, '403.html',status=403)
+    return render(request, '403.html', status=403)
 
 
 @login_required
 def custom_panel(request):
-    
+
     user = request.user
     profile = CustomerProfile.objects.filter(user=user).first()
     tickets = SupportTicket.objects.filter(sender=user).order_by('-created_at')
-    reserves = Time.objects.filter(request_reservation__user=user).order_by('-fix_reserved_date')
-    suggestions = RequestReservation.objects.filter(user=user).order_by('-suggested_reservation_date')
-    invoices = Invoice.objects.filter(customer__user=user).order_by('created_date')
+    reserves = Time.objects.filter(
+        request_reservation__user=user).order_by('-fix_reserved_date')
+    suggestions = RequestReservation.objects.filter(
+        user=user).order_by('-suggested_reservation_date')
+    invoices = Invoice.objects.filter(
+        customer__user=user).order_by('created_date')
 
     context = {
         'profile': profile,
@@ -114,34 +112,47 @@ def custom_panel(request):
         'invoices': invoices,
     }
 
-    return render(request, 'registration/custom_panel.html',context)
-    
+    return render(request, 'registration/custom_panel.html', context)
 
 
 def ticket_success(request):
-    return render (request , 'registration/ticket_success.html')
+    return render(request, 'registration/ticket_success.html')
 
 
-def custom_create_ticket(request, template_name = 'registration/customer_ticket.html'):
+def custom_create_ticket(request, template_name='registration/customer_ticket.html'):
     success = False
+    login_required_message = False
+
     if request.method == 'POST':
         if not request.user.is_authenticated:
-            request.session['pending_ticket_data'] = request.POST
-            return redirect(f"{reverse('login')}?next={request.path}")
-        form = SupportTicketForm(request.POST)
-        if form.is_valid():
-            ticket = form.save(commit=False)
-            ticket.sender = request.user
-            ticket.save()
-            success = True  
-            form = SupportTicketForm() 
-        
-    else:
-        if 'pending_ticket_data' in request.session and request.user.is_authenticated:
-            form = SupportTicket(request.session.pop('pending_ticket_data'))
+            # Save message in session & redirect
+            request.session['login_required_message'] = True
+            return redirect(request.path)
         else:
-            form = SupportTicketForm()
-    return render(request, template_name, {'form': form, 'success': success})
+            form = SupportTicketForm(request.POST)
+            if form.is_valid():
+                ticket = form.save(commit=False)
+                ticket.sender = request.user
+                ticket.save()
+                request.session['ticket_success'] = True
+                return redirect(request.path)
+    else:
+        # Ceck session messages
+        if request.session.get('login_required_message'):
+            login_required_message = True
+            del request.session['login_required_message']
+
+        if request.session.get('ticket_success'):
+            success = True
+            del request.session['ticket_success']
+
+    form = SupportTicketForm()
+
+    return render(request, template_name, {
+        'form': form,
+        'success': success,
+        'login_required_message': login_required_message
+    })
 
 
 @staff_member_required
@@ -150,20 +161,22 @@ def staff_create_ticket(request):
     success = False
     ticket_type = None
     if request.method == 'POST':
-        
+
         if 'select_ticket_type' in request.POST:
             ticket_type = request.POST.get('ticket_type')
-            form = EmployeeTicketForm(initial={'employee': request.user, 'ticket_type': ticket_type}, ticket_type=ticket_type)
+            form = EmployeeTicketForm(initial={
+                                      'employee': request.user, 'ticket_type': ticket_type}, ticket_type=ticket_type)
         else:
-            form = EmployeeTicketForm(request.POST, ticket_type=request.POST.get('ticket_type'))
+            form = EmployeeTicketForm(
+                request.POST, ticket_type=request.POST.get('ticket_type'))
             if form.is_valid():
                 ticket = form.save(commit=False)
-                ticket.employee = request.user  
+                ticket.employee = request.user
                 ticket.save()
                 return redirect('ticket_success')
     else:
         form = EmployeeTicketForm(initial={'employee': request.user})
-    return render(request, 'registration/employee_ticket.html', {'form': form, 'ticket_type': ticket_type})   
+    return render(request, 'registration/employee_ticket.html', {'form': form, 'ticket_type': ticket_type})
 
 
 @staff_member_required
@@ -171,9 +184,12 @@ def staff_create_ticket(request):
 def employee_panel(request):
     user = request.user
     profile = get_object_or_404(StaffProfile, user=user)
-    workhours = WorkHourReport.objects.filter(employee=profile).order_by('month')
-    payslips = Payslip.objects.filter(employee=profile).order_by('-date_created')
-    tickets = EmployeeTicket.objects.filter(employee=user).select_related('employee').order_by('-created_at')
+    workhours = WorkHourReport.objects.filter(
+        employee=profile).order_by('month')
+    payslips = Payslip.objects.filter(
+        employee=profile).order_by('-date_created')
+    tickets = EmployeeTicket.objects.filter(
+        employee=user).select_related('employee').order_by('-created_at')
     context = {
         'profile': profile,
         'workhours': workhours,
@@ -183,9 +199,10 @@ def employee_panel(request):
 
     return render(request, 'registration/employee_panel.html', context)
 
+
 @login_required
 def answer_custom(request):
-    
+
     user = request.user
 
     tickets = (
@@ -193,10 +210,10 @@ def answer_custom(request):
         .filter(sender=user)
         .prefetch_related('replies')
         .order_by('-created_at'))
-        
+
     # print(answers)
 
-    return render(request,'registration/answer_custom.html',{'tickets':tickets})
+    return render(request, 'registration/answer_custom.html', {'tickets': tickets})
 
 
 @staff_member_required
@@ -206,8 +223,8 @@ def answer_employee(request):
     tickets = (
         EmployeeTicket.objects
         .filter(employee=user)
-        .prefetch_related('replies') 
-        .order_by('-created_at')      
+        .prefetch_related('replies')
+        .order_by('-created_at')
     )
 
     return render(request, 'registration/answer_employee.html', {'tickets': tickets})
@@ -228,5 +245,5 @@ def suggestion(request):
     else:
         form = SuggestionForm()
 
-    back_url = 'employee_panel' if request.user.is_staff else 'custom_panel'    
-    return render(request, 'registration/suggestion.html', {'form': form, 'success': success,'back_url': back_url})
+    back_url = 'employee_panel' if request.user.is_staff else 'custom_panel'
+    return render(request, 'registration/suggestion.html', {'form': form, 'success': success, 'back_url': back_url})

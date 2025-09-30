e67b3b63dee3c6d5e2d6c76a0550ab974ab98b06
taha <tahadehghani127@gmail.com>
2025-09-30 09:16:16 +0000

    Add migrate command and run server with gunicorn

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,12 +10,22 @@ sudo chmod -R 755 /code/staticfiles /code/media
 echo "--> Collecting static files..."
 python manage.py collectstatic --noinput
 
+echo "--> Applying database migrations..."
+python manage.py migrate --noinput
+
 echo "--> Starting website"
-exec python manage.py runserver 0.0.0.0:8000
-# exec gunicorn config.wsgi:application \
-#     --workers=3 \
-#     --worker-class=gevent \
-#     --bind=0.0.0.0:8000 \
-#     --timeout=120 \
-#     --keep-alive=5 \
-#     --log-level=info
+# exec python manage.py runserver 0.0.0.0:8000
+
+exec gunicorn config.wsgi:application \
+    --workers=4 \
+    --worker-class=sync \
+    --bind=0.0.0.0:8000 \
+    --log-level=info
+
+# echo "--> Starting Uvicorn server..."
+# exec uvicorn config.asgi:application \
+#     --host 0.0.0.0 \
+#     --port 8000 \
+#     --workers 2 \
+#     --timeout-keep-alive 10
+
